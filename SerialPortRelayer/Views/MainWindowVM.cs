e6dcc59b2fcd2d52f22c4eaee6380ae4673795a3
Øystein Holvik Johnsen <oystein@holvikjohnsen.no>
2017-11-29 14:25:32 +0100

    Added window title to main window

--- SerialPortRelayer/Views/MainWindowVM.cs
+++ SerialPortRelayer/Views/MainWindowVM.cs
@@ -3,6 +3,7 @@ using SerialPortRelayer.Utilities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
@@ -10,7 +11,11 @@ using System.Windows.Input;
 namespace SerialPortRelayer.Views {
     public class MainWindowVM : BindableBase {
 
+        private const string WINDOW_TITLE = "Serial Port Relayer v";
+        private Version _version = Assembly.GetEntryAssembly().GetName().Version;
+
         private Controller _controller;
+
         public Controller Controller {
             get { return _controller; }
             set { SetNotify(ref _controller, value); }
@@ -21,6 +26,10 @@ namespace SerialPortRelayer.Views {
             _mainWindow = mainWindow;
 
             LoadCommands();
+        }
+
+        public string WindowTitle {
+            get { return WINDOW_TITLE + _version.Major + "." + _version.Minor + "." + _version.Build; }
         }
 
         private MainWindow _mainWindow;
